0e75e278e34a3a7303db243715afe5c677109d5c
Riley Seaburg <riley.seaburg@22enterprises.com>
2021-11-26 23:08:54 -0500

    Initial Concurrent Commit

--- src/main.rs
+++ src/main.rs
@@ -70,23 +70,31 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
 fn test_parse_complex() {
     assert_eq!(
         parse_complex("1.25,-0.0625"),
-        Some(Complex { re: 1.25, im: -0.0625 })
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
     );
     assert_eq!(parse_complex(",-0.0625"), None);
 }
 
 /// Given the row and column of a pixel in the output image, return the
 /// corresponding point on the complex plane.
-/// 
+///
 /// `bounds` is a pair giving the width and height of the image in pixels.
 /// `pixel` is a (column, row) pair indicating a particular pixel in that image.
 /// The `upper_left` and `lower_right` parameters are points on the complex
 /// plane designating the area our image covers.
-fn pixel_to_point(bounds: (usize,usize), 
-                  pixel: (usize,usize), 
-                  upper_left: Complex<f64>, 
-                  lower_right: Complex<f64>) -> Complex<f64> {
-    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
     Complex {
         re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
         im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
@@ -104,17 +112,25 @@ fn test_pixel_to_point() {
             Complex { re: -1.0, im: 1.0 },
             Complex { re: 1.0, im: -1.0 },
         ),
-        Complex { re: -0.5, im: -0.75 }
+        Complex {
+            re: -0.5,
+            im: -0.75
+        }
     );
 }
 
 /// Render a rectangle of the Mandelbrot set into a buffer of pixels.
-/// 
+///
 /// The `bounds` argument gives the width and height of the buffer `pixels`,
 /// which holds one grayscale pixel per byte. The `upper_left` and `lower_right`
 /// arguments specify points on the complex plane corresponding to the upper-
 /// left and lower-right corners of the pixel buffer.
-fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
     assert!(pixels.len() == bounds.0 * bounds.1);
     for row in 0..bounds.1 {
         for column in 0..bounds.0 {
@@ -127,13 +143,17 @@ fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, l
     }
 }
 
-use image::ColorType;
 use image::png::PNGEncoder;
+use image::ColorType;
 use std::fs::File;
 
 /// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
 /// file named `filename`.
-fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
     let output = File::create(filename)?;
     let encoder = PNGEncoder::new(output);
     encoder.encode(
@@ -151,8 +171,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 5 {
-        eprintln!( "Usage: {} mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
-        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
+        eprintln!(
+            "Usage: {} mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT",
+            args[0]
+        );
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
         std::process::exit(1);
     }
 
@@ -162,7 +188,27 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    // Render the entire image concurrently.
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        })
+        .unwrap();
+    }
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
